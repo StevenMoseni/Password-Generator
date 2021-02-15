e7324d54e4f7dc08ea913e0d3981f317fa1f8ba1
Steven Moseni <smoseni@hotmail.com>
2021-02-14 23:44:28 -0800

    addded prompt to event listener

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 var generateBtn = document.querySelector("#generate");
 
   function generatePassword(){
+    var finalArray = ["A", 7, "&"]
+var finalPassword = "";
     
 
 for(var i = 0; i<password.length; i++){
   var randomNumber = Math.floor(Math.random()*finalArray.length);
   finalPassword += finalArray[randomNumber]
 }
+
+  return finalPassword
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -20,3 +26,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//just a message
+prompt("Choose a length for you password of at least 8 characters and no more than 128 characters");
+//user can select OK or cancel
+var choice = confirm("Choose lowercase, uppercase, numeric, and/or special characters");
+//Stores user input in variable
+var choice2 = prompt("Choose Password");
+console.log(choice2)
+var choices = ["R","P","S"];
+//creates a random number between 0-2
+var randomIndex = Math.floor(Math.random()*3)
