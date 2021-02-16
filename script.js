71d14e11f2a31bbe53383124a91fb37463870f54
Steven Moseni <smoseni@hotmail.com>
2021-02-15 18:55:03 -0800

    updated else if

--- script.js
+++ script.js
@@ -19,8 +19,14 @@ var UpperCaseArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
 var numberArr = "0123456789" .split ("")
 var logicArr = [];
 var lowerCaseChoice = confirm("Do you want lower case?");
+var upperCaseChoice = confirm("Do you want upper case?");
 if (lowerCaseChoice) {
-}else{
+  for(var i = 0; i < lowerCaseArr.length; i++) {
+    logicArr.push(lowerCaseArr[i]) 
+  } 
+  logicArr.push(lowerCaseArr)
+
+} else if (upperCaseChoice){ 
 }
 
 var container = document.querySelector("containter");
@@ -34,6 +40,15 @@ function writePassword() {
 
 }
 
+var userChoice = 9;
+
+var logicArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!"]
+
+for(var i = 0; i , userChoice; i++){
+  finalArr.push(logicArr[Math.floor(Math.random() * logicArr.length)]);
+
+}
+console.log(finalArr.join(""))
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
