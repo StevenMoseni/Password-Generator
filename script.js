13c92006c4cf4c5fe71794143a0bf4c85d281d40
Steven Moseni <smoseni@hotmail.com>
2021-02-16 19:52:18 -0800

    added array

--- script.js
+++ script.js
@@ -19,43 +19,54 @@ var specialCharArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" .split("");
 console.log(specialCharArray);
 
 var logicArr = [];
-console.log(logicArr);
+// var finalArray = [];
 
-var passwordLength = prompt("Choose Password length between 8-128.");
+// console.log(logicArr);
+
+var passwordLength= prompt("Choose Password length between 8-128.");
 var lowerCaseChoice = confirm("Do you want lower case?");
 var upperCaseChoice = confirm("Do you want upper case?");
 var numberChoie = confirm("would you like to include numbers?");
 var SpechCharChoice =confirm ("Would you like to include special characters?");
+// var password = passwordLength.value;
 
 // console.log(passwordLength);
-var passwordLength = prompt  
-  
-}
+// console.log(passwordLengthPrompt.value)
+
+  }
 if (lowerCaseChoice) {
   for(var i = 0; i < lowerCaseArr.length; i++) {
-    logicArr.push(lowerCaseArr[i]) 
-  } 
+  logicArr.push(lowerCaseArr[i]) 
+} 
 
-
-} else if (upperCaseChoice){ 
-  for(var i = 0; i < upperCaseArr.length; i++){ 
-    logicArr.push(upperCaseArr[i])
-  }
+if (upperCaseChoice) { 
+  for(var i = 0; i < upperCaseArr.length; i++) { 
+  logicArr.push(upperCaseArr[i])
   
-} else if 
+} 
 
-for(var i = 0; i<password.length; i++){
+if (numberChoie) { 
+  for(var i = 0; i < numberArr.length; i++) { 
+  logicArr.push(numberArr[i])  
+}
+
+if (SpechCharChoice) { 
+  for(var i = 0; i < specialCharArray.length; i++) { 
+  logicArr.push(specialCharArray[i]) 
+
+};
+
+console.log(finalArray)
+
+ for(var i = 0; i<password.length; i++){
   var randomNumber = Math.floor(Math.random()*finalArray.length);
   finalPassword += finalArray[randomNumber]
+
 }
 
   return finalPassword
 }
 
-
-  
-var container = document.querySelector("containter");
-console.log(container);
 
 // Write password to the #password input
 function writePassword() {
@@ -65,28 +76,4 @@ function writePassword() {
   passwordText.value = password;
 
 }
-generateBtn.addEventListener("click", writePassword);
-// var userChoice = 9;
-
-// var logicArr = []
-// for(var i = 0; i , userChoice; i++){
-//   finalArr.push(logicArr[Math.floor(Math.random() * logicArr.length)]);
-
-// }
-// console.log(finalArr.join(""))
-
-// Add event listener to generate button
-
-
-// //just a message
-// var passwordLength = prompt("Choose Password Length");
-
-// // prompt("Choose a length for you password of at least 8 characters and no more than 128 characters");
-// // //user can select OK or cancel
-// var choice = confirm("Choose lowercase, uppercase, numeric, and/or special characters");
-// //Stores user input in variable
-// // var choice2 = prompt("Choose Password");
-// console.log(choice2)
-// var choices = ["R","P","S"];
-// //creates a random number between 0-2
-// var randomIndex = Math.floor(Math.random()*3)
+generateBtn.addEventListener("click", writePassword)
