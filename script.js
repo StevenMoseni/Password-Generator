59776560c57ff3338c5e494cbd69acd5ba78f1c2
Steven Moseni <smoseni@hotmail.com>
2021-02-16 21:41:47 -0800

    fixed Password functions, ccompleted code

--- script.js
+++ script.js
@@ -2,71 +2,65 @@
 var generateBtn = document.querySelector("#generate");
 console.log(generateBtn);
 
-  function generatePassword(){
-    var finalArray = [];
-var finalPassword = "";
-    
-var lowerCaseArr = "abcdefghijklmnopqrstuvwxyz" .split("");
-console.log(lowerCaseArr);
+function generatePassword() {
+  var finalPassword = "";
 
-var upperCaseArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
-console.log(upperCaseArr);
+  var lowerCaseArr = "abcdefghijklmnopqrstuvwxyz".split("");
+  console.log(lowerCaseArr);
 
-var numberArr = "0123456789" .split("");
-console.log(numberArr);
+  var upperCaseArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  console.log(upperCaseArr);
 
-var specialCharArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" .split("");
-console.log(specialCharArray);
+  var numberArr = "0123456789".split("");
+  console.log(numberArr);
 
-var logicArr = [];
-// var finalArray = [];
+  var specialCharArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+  console.log(specialCharArray);
 
-// console.log(finalPassword);
+  var logicArr = [];
 
-var passwordLength= prompt("Choose Password length between 8-128.");
-var lowerCaseChoice = confirm("Do you want lower case?");
-var upperCaseChoice = confirm("Do you want upper case?");
-var numberChoice = confirm("would you like to include numbers?");
-var SpechCharChoice =confirm ("Would you like to include special characters?");
-// var lengthChoice = pas
+  var passwordLength = prompt("Choose Password length between 8-128.");
+  var lowerCaseChoice = confirm("Do you want lower case?");
+  var upperCaseChoice = confirm("Do you want upper case?");
+  var numberChoice = confirm("would you like to include numbers?");
+  var SpechCharChoice = confirm("Would you like to include special characters?");
 
-console.log(passwordLength);
+  console.log(passwordLength);
 
-
-if (lowerCaseChoice) {
-  for(var i = 0; i < lowerCaseArr.length; i++) {
-  logicArr.push(lowerCaseArr[i]) 
-  } 
-}
-
-if (upperCaseChoice) { 
-  for(var i = 0; i < upperCaseArr.length; i++) { 
-  logicArr.push(upperCaseArr[i])
+  if (lowerCaseChoice) {
+    for (var i = 0; i < lowerCaseArr.length; i++) {
+      logicArr.push(lowerCaseArr[i])
+    }
   }
-} 
-
-if (numberChoice) { 
-  for(var i = 0; i < numberArr.length; i++) { 
-  logicArr.push(numberArr[i])  
+  console.log("logicArr", logicArr);
+  if (upperCaseChoice) {
+    for (var j = 0; j < upperCaseArr.length; j++) {
+      logicArr.push(upperCaseArr[j])
+    }
   }
-}
-
-if (SpechCharChoice) { 
-  for(var i = 0; i < specialCharArray.length; i++) { 
-  logicArr.push(specialCharArray[i]) 
+  console.log("logicArr", logicArr);
+  if (numberChoice) {
+    for (var h = 0; h < numberArr.length; h++) {
+      logicArr.push(numberArr[h])
+    }
   }
-}
+  console.log("logicArr", logicArr);
+  if (SpechCharChoice) {
+    for (var n = 0; n < specialCharArray.length; n++) {
+      logicArr.push(specialCharArray[n])
+    }
+  }
+  console.log("logicArr", logicArr);
+  
+  for (var i = 0; i < passwordLength; i++) {
+    console.log("interation: ", i);
+    var randomNumber = Math.floor(Math.random() * logicArr.length);
+    console.log("random number: ", randomNumber);
+    finalPassword += logicArr[randomNumber]
 
-console.log(finalArray)
-
- for(var i = 0; i<password.length; i++){
-  var randomNumber = Math.floor(Math.random()*finalArray.length);
-  finalPassword += finalArray[randomNumber]
-
-}
-
-  return finalPassword
+  }
   console.log(finalPassword);
+  return finalPassword
 }
 
 
